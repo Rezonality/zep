12172390543b6470afbd1f148f14ff917671f372
Chris Maughan <cmaughan@snarlsoftware.com>
2019-10-12 10:45:27 +0100

    First attempt at fixing auto builds

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 if test "${BUILD_TYPE}" = "Coverity"; then
-  source ./prebuild.sh
+  git clone https://github.com/cmaughan/MUtils
+  cd MUtils
+  chmod +x prebuild.sh
+  chmod +x m3rdparty/packages/install.sh
+  ./prebuild.sh
+  mkdir build
+  cd build
+  cmake .. \
+    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+  cmake --build . --target install
+  cd ..
+  cd ..
   mkdir build
   cd build
   cmake .. \
@@ -10,13 +22,24 @@ if test "${BUILD_TYPE}" = "Coverity"; then
     -DPROJECT_UNITTESTS=no
   bash <(curl -s https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh)
 else
-  source ./prebuild.sh
+  git clone https://github.com/cmaughan/MUtils
+  cd MUtils
+  chmod +x prebuild.sh
+  chmod +x m3rdparty/packages/install.sh
+  ./prebuild.sh
+  mkdir build
+  cd build
+  cmake .. \
+    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+  cmake --build . --target install
+  cd ..
+  cd ..
   mkdir build
   cd build
   cmake .. \
     -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} \
     -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} \
-    -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
-  cmake --build .
+    -DPROJECT_UNITTESTS=no
   cmake --build . --target ${TEST_TARGET} -- ARGS=--output-on-failure
 fi
