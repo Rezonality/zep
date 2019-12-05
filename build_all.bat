d2c6b441e120d40bfe9e3749d82993c3da00d915
Chris Maughan <cmaughan@snarlsoftware.com>
2019-12-05 22:03:32 +0000

    fix rel+dbg

--- build_all.bat
+++ build_all.bat
@@ -2,4 +2,5 @@ call config_all.bat
 cd build
 cmake --build . --config Release --target Install
 cmake --build . --config Debug --target Install
+cmake --build . --config RelWithDebInfo --target Install
 cd ..
