4e9bcd58b742fbf9ee788fcd35e7db2205e24d80
cmaughan <mornymorny@gmail.com>
2020-10-23 11:13:22 +0100

    Remove old file

--- cmake/all.cmake
+++ cmake/all.cmake
@@ -70,21 +70,11 @@ endif()
 if("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
     message(STATUS "TARGET_MAC")
     
-    find_package(OpenGL REQUIRED)
     find_package(Threads REQUIRED)
-    find_package(BZIP2 REQUIRED)
+    find_package(BZip2 REQUIRED)
     find_package(ZLIB REQUIRED)
 
-    if(NOT OPENGL_FOUND)
-        message(ERROR " OPENGL not found!")
-    endif(NOT OPENGL_FOUND)
-
-    include_directories(${OpenGL_INCLUDE_DIRS})
-    link_directories(${OpenGL_LIBRARY_DIRS})
-    add_definitions(${OpenGL_DEFINITIONS})
-
     LIST(APPEND PLATFORM_LINKLIBS
-        ${OPENGL_LIBRARY}
         ${BZIP2_LIBRARY}
         ${ZLIB_LIBRARY}
         dl
@@ -95,23 +85,13 @@ if("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
 endif()
 
 if("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
-    find_package(OpenGL REQUIRED)
-    if(NOT OPENGL_FOUND)
-        message(ERROR " OPENGL not found!")
-    endif(NOT OPENGL_FOUND)
-
     set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
     set(THREADS_PREFER_PTHREAD_FLAG TRUE)
     find_package(Threads REQUIRED)
 
-    include_directories(${OpenGL_INCLUDE_DIRS})
-    link_directories(${OpenGL_LIBRARY_DIRS})
-    add_definitions(${OpenGL_DEFINITIONS})
-
     LIST(APPEND PLATFORM_LINKLIBS
         dl 
         Threads::Threads
-        ${OPENGL_LIBRARY}
         z
         )
 
--- cmake/pc.cmake
+++
@@ -1,6 +0,0 @@
-LIST(APPEND PLATFORM_LINKLIBS
-    opengl32.lib
-    winmm.lib   # SDL - sound, etc.
-    version.lib # SDL - windows keyboard
-    imm32.lib   # SDL - windows keyboard
-)
